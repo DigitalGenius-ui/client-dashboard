640f6ee7e627da896bd433babf23fa7addaa161c
Milad Amiri <miladmarketing282.com>
2022-10-05 15:42:49 +0800

    added

---
+++ javascript/role.js
@@ -0,0 +1,51 @@
+const permissionDropDown = document.querySelector(".permissionDropDown");
+const roleDropDown = document.querySelector(".roleDropDown");
+
+$(permissionDropDown)
+    .dropdown({
+    clearable: true
+});
+
+$(roleDropDown)
+    .dropdown({
+    clearable: true
+});
+
+// table data handler 
+const addTable = document.querySelector(".addTable");
+
+const tableData = [
+    {
+        id : 1,
+        permission : "Fully Control",
+        role : "Admin",
+        netId : "gst2209@polyu.edu.hk"
+    },
+    {
+        id : 2,
+        permission : "View all orders",
+        role : "Assistance",
+        netId : "gst2209@polyu.edu.hk"
+    },
+    {
+        id : 3,
+        permission : "View personal orders only",
+        role : "Normal User",
+        netId : "gst2209@polyu.edu.hk"
+    },
+]
+
+const showData = tableData.map((item) => {
+    return `
+        <tr class="tableRow">
+            <td class="action">
+              <span class="ti-trash"></span>
+              <span class="ti-pencil-alt"></span>
+            </td>
+            <td>${item.permission}</td>
+            <td>${item.role}</td>
+            <td>${item.netId}</td>
+        </tr>
+    `
+});
+addTable.insertAdjacentHTML("beforebegin", showData.join(" "))
